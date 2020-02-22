6a9628c57f7fb89bf152c69390ecb8233ca63c70
Tacobell <chessegrater@gmail.com>
2020-02-22 17:28:27 -0500

    Push 3

--- aesthetics.py
+++ aesthetics.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def scatterwords_index():
-    return render_template('index.html', template_folder='slick')
+    return render_template('index.html')
 
 if __name__ == "main":
     app.run(debug=True)
