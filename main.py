7b011c90bb179d4197618e33f48e5a9995817374
Tacobell <chessegrater@gmail.com>
2020-02-22 18:49:06 -0500

    Push 5
    this has the progress for the counting logic

--- main.py
+++ main.py
@@ -33,6 +33,11 @@ def server_error(e):
     """.format(e), 500
 
 
+
+
+
+
+
 if __name__ == '__main__':
     # This is used when running locally. Gunicorn is used to run the
     # application on Google App Engine. See entrypoint in app.yaml.
---
+++ test.py
@@ -0,0 +1,15 @@
+def word_count():
+    f = 'This is a test to see if I can make a count dictionary. This is this because this is this.'
+    l = list(map(lambda  x: x, f.split()))
+    return l
+
+def dictionary(list):
+    repeating_words = {}
+    for things in range(list):
+        if list[things] in list:
+            
+
+'''<p>'''
+
+
+word_count()
