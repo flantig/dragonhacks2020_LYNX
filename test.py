872988c9d9611fdcebdb2097df36fefb6755b0ab
Tacobell <chessegrater@gmail.com>
2020-02-22 20:20:18 -0500

    Push again

--- test.py
+++ test.py
@@ -1,15 +1,21 @@
-def word_count():
-    f = 'This is a test to see if I can make a count dictionary. This is this because this is this.'
-    l = list(map(lambda  x: x, f.split()))
+from collections import Counter
+
+f = 'This is a test to see if I can make a count dictionary. This is this because this is this.'
+
+def word_list(y):
+    l = list(map(lambda  x: x, y.split()))
     return l
 
-def dictionary(list):
-    repeating_words = {}
-    for things in range(list):
-        if list[things] in list:
-            
+def word_counter(list):
+    cntin = Counter()
+    for word in list:
+        cntin[word] += 1
+    return cntin
 
-'''<p>'''
+banned_words = []
+
+list_of_word = word_list(f)
+most_common_words = Counter(word_list(f)).most_common(10)
+print(most_common_words)
 
 
-word_count()
